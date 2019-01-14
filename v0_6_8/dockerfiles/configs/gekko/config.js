e98b021c4fe12b9a1699f1794867540b673021ab
vsilent <jabberroid@gmail.com>
2019-01-14 18:31:40 +0200

    running on port 3000

---
+++ v0_6_8/dockerfiles/configs/gekko/config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+  headless: true,
+  api: {
+    host: '0.0.0.0',
+    port: 3000,
+    timeout: 120000 // 2 minutes
+  },
+  ui: {
+    ssl: false,
+    host: '0.0.0.0',
+    port: 3000,
+    path: '/'
+  },
+  adapter: 'sqlite'
+}
+
+
+if(typeof window === 'undefined')
+  module.exports = CONFIG;
+else
+  window.CONFIG = CONFIG;
